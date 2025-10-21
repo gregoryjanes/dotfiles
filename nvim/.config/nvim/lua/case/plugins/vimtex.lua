f638dc6c0510a15e89909adc28fb55049e354712
Gregory Janes <janesjo@mail.gvsu.edu>
2025-10-21 11:58:57 -0400

    updated lua plugins

--- nvim/.config/nvim/lua/case/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/case/plugins/vimtex.lua
@@ -1,9 +1,30 @@
+
+
 return {
   "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
+  lazy = false,
   init = function()
-    -- VimTeX configuration goes here, e.g.
+    -- Set your PDF viewer (e.g., "skim", "zathura", "okular")
     vim.g.vimtex_view_method = "skim"
-  end
+
+    -- Set the main compiler to latexmk
+    vim.g.vimtex_compiler_method = "latexmk"
+
+    -- Configure latexmk to use lualatex as its engine
+    vim.g.vimtex_compiler_latexmk_engines = {
+      _ = "-lualatex"
+    }
+      
+    -- Set the bibliography tool for biblatex
+    vim.g.vimtex_bibtex_engine = "biber"
+    vim.g.vimtex_compiler_latexmk_options = '-pdf -bibtex-use=2'
+
+    -- Define files to be removed by :VimtexClean
+    vim.g.vimtex_clean_exts = {
+      "aux", "bbl", "bcf", "blg", "idx", "ind", "ilg", "log", "out",
+      "run.xml", "synctex.gz", "toc", "nav", "snm", "vrb",
+    }
+    
+    vim.g.vimtex_clean_patterns = { "*-blx.bib", "_minted*" }
+  end,
 }
--- nvim/.config/nvim/lua/case/plugins/which-key.lua
+++ nvim/.config/nvim/lua/case/plugins/which-key.lua
@@ -1,13 +1,121 @@
+-- File: ~/.config/nvim/lua/case/plugins/which-key.lua
+
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
+  dependencies = {
+    'echasnovski/mini.nvim',
+  },
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+    preset = "classic",
+    delay = function(ctx)
+      return ctx.plugin and 0 or 200
+    end,
+    show_help = false,    -- Remove bottom help/status bar
+    show_keys = false,    -- Remove key hints
+    win = {
+      border = "rounded",
+      padding = { 1, 2 },
+      title = false,
+      title_pos = "center",
+      zindex = 1000,
+      wo = {
+        winblend = 10,
+      },
+      bo = {
+        filetype = "which_key",
+        buftype = "nofile",
+      },
+    },
+    icons = {
+      breadcrumb = "»",
+      separator = "➜",
+      group = "+",
+    },
+    layout = {
+      width = { min = 20, max = 50 },
+      height = { min = 4, max = 25 },
+      spacing = 3,
+      align = "left",
+    },
+    keys = {
+      scroll_down = "<c-d>",
+      scroll_up = "<c-u>",
+    },
+    sort = { "local", "order", "group", "alphanum", "mod" },
+    disable = {
+      bt = { "help", "quickfix", "terminal", "prompt" },
+      ft = { "neo-tree" }
+    },
+    triggers = {
+      { "<leader>", mode = { "n", "v" } }
+    }
+},
+  config = function(_, opts)
+    -- This config function runs after the plugin is loaded
+    local wk = require("which-key")
+    wk.setup(opts)
+
+    -- =========================================================================
+    -- SECTION 1: GLOBAL KEYMAPS
+    -- These keymaps will be available in ANY file type.
+    -- =========================================================================
+
+    -- Group for general file/window management
+    wk.add({
+      { "<leader>q", "<cmd>qa!<CR>", desc = "Quit Neovim", icon = "󰗼" },
+      { "<leader>w", "<cmd>wa!<CR>", desc = "Write All Files", icon = "󰆓" },
+      { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle File Explorer", icon = "󰙅" },
+    })
+
+    -- Group for Telescope (Find)
+    wk.add({
+      { "<leader>f", group = "Find (Telescope)", icon = "󰍉" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files", icon = "󰈙" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find Text (Grep)", icon = "󰊄" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers", icon = "󰓩" },
+      -- THIS IS THE BIBTEX KEYMAP YOU WANTED!
+      -- It's global, but will only work if a .bib file is found by the bibtex extension.
+      { "<leader>fc", "<cmd>Telescope bibtex<cr>", desc = "Find Citations", icon = "󰈙" },
+    })
+
+    -- =========================================================================
+    -- SECTION 2: FILETYPE-SPECIFIC KEYMAPS
+    -- PATTERN: Use autocommands to register keymaps only for specific filetypes.
+    -- This keeps your keybindings organized and context-aware.
+    -- =========================================================================
+
+    -- LaTeX / VimTeX Keymaps
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "tex", "latex", "bib" }, -- Trigger on these file types
+      callback = function()
+        -- These mappings are created ONLY when you open a file matching the pattern above.
+        wk.add({
+          buffer = 0, -- Apply these keymaps only to the current buffer
+          { "<leader>l", group = "LaTeX (VimTeX)", icon = "󰙩" },
+          { "<leader>lc", "<cmd>VimtexCompile<CR>", desc = "Compile", icon = "󰖷" },
+          { "<leader>lv", "<cmd>VimtexView<CR>", desc = "View PDF", icon = "󰛓" },
+          { "<leader>le", "<cmd>VimtexErrors<CR>", desc = "Show Errors", icon = "󰅚" },
+          { "<leader>li", "<cmd>VimtexTocOpen<CR>", desc = "Table of Contents", icon = "󰋽" },
+          { "<leader>lk", "<cmd>VimtexClean<CR>", desc = "Clean Aux Files", icon = "󰩺" },
+        })
+      end,
+    })
+
+    -- Example: Python Keymaps
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "python" },
+      callback = function()
+        wk.add({
+          buffer = 0,
+          { "<leader>r", group = "Run Python", icon = "󰌠" },
+          { "<leader>rr", "<cmd>!python %<CR>", desc = "Run Current File", icon = "󰐊" },
+        })
+      end,
+    })
+
+    -- Add more autocommands here for other filetypes like markdown, lua, etc.
+  end,
 }
+
+
